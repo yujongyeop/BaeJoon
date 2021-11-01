13334bc8aca65c1d841090ec3aad4a8741174e9d
platform1117 <230233@naver.com>
2021-11-01 11:27:47 +0900

    숨바꼭질6

---
+++ BAEJOON/src/Algorithm_Basic/hide_and_seek6.java
@@ -0,0 +1,36 @@
+package Algorithm_Basic;
+
+//https://www.acmicpc.net/problem/17087
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class hide_and_seek6 {
+	static int gcd(int a, int b) {// a를 b로 나눴을 때 나머지가 0이 될 때까지 재귀호출(나머지가 0일 경우의 b값이 최대공약수이다
+		return a % b != 0 ? gcd(b, a % b) : b;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] input = br.readLine().split(" ");
+		String[] pos = br.readLine().split(" ");
+		int num = Integer.parseInt(input[0]);
+		int[] x = new int[num];
+		int S = Integer.parseInt(input[1]);
+		for (int i = 0; i < num; i++)
+			x[i] = Math.abs(Integer.parseInt(pos[i]) - S);
+		int ans = x[0];
+		for (int i = 1; i < num; i++) {
+			ans = gcd(ans, x[i]);// 가장 작은 수와 최소 공배수를 구함
+			// 사실 이 부분이 잘 이해가 안되네요
+		}
+		bw.write(Integer.toString(ans));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
